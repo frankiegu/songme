15ba48482e5d4752dc64f276049bb2edb20fd9f5
emredemir <aemdemir@gmail.com>
2018-02-06 03:00:42 +0300

    Retrieves port from env

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/emre-demir/songme/common/env"
 	"github.com/emre-demir/songme/controllers"
@@ -10,6 +11,7 @@ import (
 )
 
 func main() {
+	// Database
 	db, err := datastore.NewDatastore(datastore.PostgreSQL, datastore.DefaultConfig)
 	if err != nil {
 		log.Fatal("[MAIN - DB]:", err)
@@ -21,8 +23,17 @@ func main() {
 	// FileServer
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 
+	// Routes
 	http.HandleFunc("/", controllers.Index)
 	http.Handle("/signup", controllers.SignUp(env))
 
-	http.ListenAndServe(":8080", nil)
+	// Serve
+	port := "8080"
+	if os.Getenv("ENV") == "PRODUCTION" {
+		port = os.Getenv("PORT")
+		if port == "" {
+			log.Fatal("MAIN - PORT:", err)
+		}
+	}
+	http.ListenAndServe(":"+port, nil)
 }
