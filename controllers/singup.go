157dcfd471e9826af3819cac3ea5944709d5e30d
emredemir <aemdemir@gmail.com>
2018-02-07 02:26:05 +0300

    Shows error messages on index page

--- controllers/singup.go
+++ controllers/singup.go
@@ -9,6 +9,14 @@ import (
 	"github.com/emre-demir/songme/models"
 )
 
+// indexPage contains information about index page.
+type indexPage struct {
+	Title        string
+	ElementNames []string
+	Fields       map[string]string
+	Errors       map[string]string
+}
+
 // SignUp registers new subscriber
 func SignUp(e *env.Env) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -17,18 +25,35 @@ func SignUp(e *env.Env) http.Handler {
 			w.WriteHeader(http.StatusNotFound)
 			RenderTemplate(w, "error/page", nil)
 		case "POST":
-			signup(w, r, e)
+			p := &indexPage{
+				Title:        "Songme",
+				ElementNames: []string{"name", "email"},
+				Fields:       make(map[string]string),
+				Errors:       make(map[string]string),
+			}
+			for _, element := range p.ElementNames {
+				p.Fields[element] = r.FormValue(element)
+			}
+			signup(w, r, e, p)
 		}
 	})
 }
 
 // signup validates and handles posted singup form
-func signup(w http.ResponseWriter, r *http.Request, e *env.Env) {
+func signup(w http.ResponseWriter, r *http.Request, e *env.Env, p *indexPage) {
 	name := r.FormValue("name")
 	email := r.FormValue("email")
 
-	if strings.TrimSpace(name) == "" || strings.TrimSpace(email) == "" {
-		http.Redirect(w, r, "/", http.StatusBadRequest)
+	if strings.TrimSpace(name) == "" {
+		p.Errors["name"] = "Please enter a valid name."
+	}
+
+	if strings.TrimSpace(email) == "" {
+		p.Errors["email"] = "Please enter a valid email."
+	}
+
+	if len(p.Errors) > 0 {
+		RenderTemplate(w, "index/home", &p)
 		return
 	}
 
