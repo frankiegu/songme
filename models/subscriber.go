be21921b146623043b8bbfe91b8291df5ec411aa
emredemir <aemdemir@gmail.com>
2018-02-07 05:34:59 +0300

    Added constructor

--- models/subscriber.go
+++ models/subscriber.go
@@ -4,10 +4,18 @@ import (
 	"time"
 )
 
-// Subscriber represents our newsletter subscribers
+// Subscriber represents our newsletter subscribers.
 type Subscriber struct {
 	ID        int
 	Name      string
 	Email     string
 	CreatedAt time.Time
+}
+
+// NewSubscriber returns new subscriber.
+func NewSubscriber(name, email string) *Subscriber {
+	return &Subscriber{
+		Name:  name,
+		Email: email,
+	}
 }
