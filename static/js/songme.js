b5477003eb7f9a4f4050d802c25816038bc19780
emredemir <aemdemir@gmail.com>
2018-02-20 04:16:24 +0300

    Turn url to embed codes

--- static/js/songme.js
+++ static/js/songme.js
@@ -1,8 +1,17 @@
 
-$('a.embed-link').each(function(index, element){
-    $.getJSON('https://noembed.com/embed', {format: 'json', url: element.href}, function (response) {
-      $(element).replaceWith(response.html);
-    }).fail(function (){
-        console.log('Can not fetch embed code');
-    });
+// Runs when document ready.
+$( document ).ready(function() {
+    replaceEmbedCodes();
 });
+
+// replaceEmbedCodes finds each anchor element with class embed-link
+// and requests for embed codes from url that anchor element points.
+function replaceEmbedCodes() {
+    $('a.embed-link').each(function(index, element){
+        $.getJSON('https://noembed.com/embed', {format: 'json', url: element.href}, function (response) {
+          $(element).replaceWith(response.html);
+        }).fail(function (){
+            console.log('Can not fetch embed code');
+        });
+    });
+}
