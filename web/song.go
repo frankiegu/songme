91930f445d5777f211f9449606b83c4f2263593c
emredemir <aemdemir@gmail.com>
2018-06-05 23:16:04 +0300

    refactor-features 11: Implement song related handler and controllers

--- web/song.go
+++ web/song.go
@@ -2,85 +2,76 @@ package web
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/emredir/songme/models"
+
 	"github.com/gorilla/mux"
-	"github.com/lib/pq"
 )
 
-type songAddViewData struct {
-	Form *models.Form
-}
-
-type songSuccessViewData struct {
-	Song *models.Song
-}
-
-type homeViewData struct {
-	Song *models.Song
+// SongInteractor is used to interact with the database for song related tasks.
+type SongInteractor interface {
+	models.SongStore
 }
 
 // SongHandler defines song specific controllers.
 type SongHandler struct {
-	songStore models.SongStore
-}
-
-// Index renders the home page with a randomly selected song.
-func (h *SongHandler) Index(w http.ResponseWriter, r *http.Request) {
-	s, _ := h.songStore.GetRandom()
-	RenderTemplate(w, "index/home", &homeViewData{Song: s})
+	songInteractor    SongInteractor
+	descriptionLength int
+	songsPerPage      int
 }
 
 // New renders the form for creating a new song.
 func (h *SongHandler) New(w http.ResponseWriter, r *http.Request) {
-	RenderTemplate(w, "song/add", nil)
+	view := NewView(r)
+	view.Render(w, "song/recommend")
 }
 
 // Create creates a new song.
 func (h *SongHandler) Create(w http.ResponseWriter, r *http.Request) {
-	f := models.NewForm([]string{"title", "author", "songURL", "imageURL", "description"})
+	view := NewView(r)
 
-	f.Populate(r)
-	f.CheckRequiredFields([]string{"title", "author", "songURL"})
-	ok := f.IsValid()
-	if !ok {
-		RenderTemplate(w, "song/add", &songAddViewData{Form: f})
+	title := view.FormValue("title", true)
+	artist := view.FormValue("artist", true)
+	songURL := view.FormValue("songURL", true)
+	imageURL := view.FormValue("imageURL", false)
+	description := view.FormValue("description", false)
+
+	if view.HasError() {
+		view.Render(w, "song/recommend")
+		return
+	}
+
+	if len(description) > h.descriptionLength {
+		view.InsertFlashError("Descriptions must be at most ", h.descriptionLength, " characters long")
+		view.Render(w, "song/recommend")
 		return
 	}
 
 	s := &models.Song{
-		Title:       r.FormValue("title"),
-		Author:      r.FormValue("author"),
-		SongURL:     r.FormValue("songURL"),
-		ImageURL:    r.FormValue("imageURL"),
-		Description: r.FormValue("description"),
+		Title:       title,
+		Artist:      artist,
+		SongURL:     songURL,
+		ImageURL:    &imageURL,
+		Description: &description,
 	}
-	err := h.songStore.Create(s)
-	if err == nil {
-		RenderTemplate(w, "song/success", &songSuccessViewData{Song: s})
+	err := h.songInteractor.Create(s)
+	if err != nil {
+		view.InsertFlashError(err.Error())
+		view.Render(w, "song/recommend")
 		return
 	}
 
-	// Check for the type of postgresql error.
-	if pqError, ok := err.(*pq.Error); ok {
-		// If error is because of unique violation, then inform user.
-		if pqError.Code == "23505" {
-			f.Errors["alreadyExists"] = "Opps! Sorry, song already exists."
-			RenderTemplate(w, "song/add", &songAddViewData{Form: f})
-			return
-		}
-	}
-
-	w.WriteHeader(http.StatusInternalServerError)
-	RenderTemplate(w, "error/page", nil)
+	view.InsertSong(s)
+	view.Render(w, "song/success")
 }
 
-// Confirm transfers song from table 'candidate_song' to 'production_song'.
+// Confirm sets confirmed true for a song.
 func (h *SongHandler) Confirm(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	id := vars["id"]
 
-	err := h.songStore.Confirm(id)
+	err := h.songInteractor.Confirm(id)
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 		return
@@ -89,57 +80,61 @@ func (h *SongHandler) Confirm(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 }
 
-// Candidates renders all candidate songs.
+// Songs returns all songs that are currently confirmed.
+func (h *SongHandler) Songs(w http.ResponseWriter, r *http.Request) {
+	view := NewView(r)
+
+	page := 1
+	vars := mux.Vars(r)
+	val, ok := vars["page"]
+	if ok {
+		var err error
+		page, err = strconv.Atoi(val)
+		if err != nil {
+			view.InsertFlashError("Page ", val, " cannot be found")
+			view.Render(w, "song/all")
+			return
+		}
+	}
+
+	offset := (page - 1) * h.songsPerPage
+	songs, totalCount, err := h.songInteractor.All(true, h.songsPerPage, offset)
+	if err != nil {
+		view.InsertFlashError(err.Error())
+		view.Render(w, "song/all")
+		return
+	}
+
+	pagination := newPagination(totalCount, h.songsPerPage, 10, page)
+	view.InsertSongs(songs)
+	view.InsertPagination(pagination)
+	view.Render(w, "song/all")
+}
+
+// Candidates returns all songs that are currently not confirmed.
 func (h *SongHandler) Candidates(w http.ResponseWriter, r *http.Request) {
-	songs, err := h.songStore.Candidates()
+	view := NewView(r)
+
+	songs, _, err := h.songInteractor.All(false, 100, 0)
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
 
-	data := make(map[string]interface{})
-	data["Songs"] = songs
-
-	RenderTemplate(w, "song/rows", data)
+	view.InsertSongs(songs)
+	view.Render(w, "song/rows")
 }
 
-// Productions renders all songs in production.
-func (h *SongHandler) Productions(w http.ResponseWriter, r *http.Request) {
-	songs, err := h.songStore.Productions()
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-
-	data := make(map[string]interface{})
-	data["Songs"] = songs
-	data["Hidden"] = map[string]bool{"selectButton": true}
-
-	RenderTemplate(w, "song/rows", data)
-}
-
-// DeleteCandidate deletes song from table 'candidate_song'.
-func (h *SongHandler) DeleteCandidate(w http.ResponseWriter, r *http.Request) {
+// Delete deletes a song from the database.
+func (h *SongHandler) Delete(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	id := vars["id"]
 
-	err := h.songStore.DeleteCandidate(id)
+	err := h.songInteractor.Delete(id)
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
-	w.WriteHeader(http.StatusOK)
-}
 
-// DeleteProduction deletes song from table 'production_song'.
-func (h *SongHandler) DeleteProduction(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	id := vars["id"]
-
-	err := h.songStore.DeleteProduction(id)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
 	w.WriteHeader(http.StatusOK)
 }
